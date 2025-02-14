a0adf9c6f9f1407b754b31af93636397f3534bb4
DavidCa25 <banerdavid29@gmail.com>
2025-02-14 11:02:05 -0600

    [Fix] Validación conexión base de datos

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import app from './app';
 import 'reflect-metadata';
+import DatabaseConnection from './databsase/DataBaseConnection';// Importa la clase que creaste
 
 const port = 3001;
 
-app.listen(port, () =>{
-    console.log(`app listening on port ${port}`);
-})
+async function startServer() {
+  try {
+    await DatabaseConnection.getConnectedInstance(); 
+    console.log("Conexión a la base de datos exitosa");
+
+    app.listen(port, () => {
+      console.log(`Servidor corriendo en el puerto ${port}`);
+    });
+  } catch (error) {
+    console.error("Error al conectar a la base de datos:", error);
+    process.exit(1); 
+  }
+}
+
+startServer();
